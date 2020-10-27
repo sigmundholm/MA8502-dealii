c0880352af43879b5ce191bf6a55749990546ee4
Sigmund Eggen Holm <sigmund.holm@gmail.com>
2020-10-27 09:40:31 +0100

    Name the graphs.

--- src/streamline_diffusion/conv_plot.py
+++ src/streamline_diffusion/conv_plot.py
@@ -1,21 +1,25 @@
 import os
 from os.path import split
 import numpy as np
+import matplotlib.pyplot as plt
 
 from utils.plot import conv_plots
 
 if __name__ == '__main__':
     base = split(split(os.getcwd())[0])[0]
-    skip = 0
+    skip = 1
 
-    rho = 1
-    for poly_order in [2, 3]:
-        full_path = os.path.join(base, f"build/src/streamline_diffusion/errors-o{poly_order}-eps=0.100000-rho={rho}.csv")
+    rho = -1
+    eps = "0.100000"
+    names = {-1: "Douglas-Wang", 0: "Streamline Diffusion", 1: "Galerkin Least Squares"}
+    for poly_order in [1, 2]:
+        full_path = os.path.join(base, f"build/src/streamline_diffusion/errors-o{poly_order}-eps={eps}-rho={rho}.csv")
 
         head = list(map(str.strip, open(full_path).readline().split(",")))
         data = np.genfromtxt(full_path, delimiter=",", skip_header=True)
         data = data[skip:, :]
 
-        conv_plots(data, head, title=r"$\textrm{Streamline Diffusion: "
+        conv_plots(data, head, title=r"$\textrm{" + names[rho] + ": "
                                      r"polynomial order: " + str(poly_order) + "}$"
-                                     r", $\epsilon=0.1, \rho=" + str(rho) + "$", latex=True)
+                                     r", $\epsilon=" + str(float(eps)) + r"$, $\rho=" + str(rho) + "$", latex=True)
+    plt.show()
