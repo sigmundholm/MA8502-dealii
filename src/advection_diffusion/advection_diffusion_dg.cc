455959b1f734f21f11d1cc77dcac3100116278e4
Sigmund Eggen Holm <sigmund.holm@gmail.com>
2020-10-29 13:56:49 +0100

    Move main to separate file.

--- src/advection_diffusion/advection_diffusion_dg.cc
+++ src/advection_diffusion/advection_diffusion_dg.cc
@@ -229,19 +229,5 @@ void AdvectionDiffusionDG<dim>::assemble_system() {
 
 }
 
-
-int main() {
-    std::cout << "AdvectionDiffusionDG" << std::endl;
-    {
-        double eps = 0.1;
-        RightHandSideAD<2> rhs(eps);
-        BoundaryValuesAD<2> boundary_values(eps);
-        AnalyticalSolutionAD<2> analytic(eps);
-
-        AdvectionDiffusionDG<2> advec_diff(1, 5, eps, rhs, boundary_values,
-                                           analytic);
-        advec_diff.run();
-    }
-
-
-}
+template
+class AdvectionDiffusionDG<2>;
