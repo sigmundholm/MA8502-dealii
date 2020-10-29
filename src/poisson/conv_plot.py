1044af5078808c659e201899d398ff37b666dc85
Sigmund Eggen Holm <sigmund.holm@gmail.com>
2020-10-29 17:45:41 +0100

    Create convergence test and plot for Poisson DG.

--- src/poisson/conv_plot.py
+++ src/poisson/conv_plot.py
@@ -2,15 +2,17 @@ import os
 from os.path import split
 import numpy as np
 
+import matplotlib.pyplot as plt
 from utils.plot import conv_plots
 
 if __name__ == '__main__':
     base = split(split(os.getcwd())[0])[0]
-    for poly_order in [2, 3]:
-        full_path = os.path.join(base, f"build/src/poisson/errors-o{poly_order}.csv")
+    for poly_order in [1, 2]:
+        full_path = os.path.join(base, f"build/src/poisson/errors-o{poly_order}-dg.csv")
 
         head = list(map(str.strip, open(full_path).readline().split(",")))
         data = np.genfromtxt(full_path, delimiter=",", skip_header=True)
 
         conv_plots(data, head, title=r"$\textrm{Poisson: polynomial order: " + str(poly_order) + "}$",
                    latex=True)
+    plt.show()
